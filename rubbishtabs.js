eeab097e8070673fd8f39c5bfb1db69e43d8d0de
Jack Franklin <jack@jackfranklin.net>
2013-06-03 20:26:57 +0100

    Add activateLink method

--- rubbishtabs.js
+++ rubbishtabs.js
@@ -1,4 +1,10 @@
 var tabularize = function() {
+
+  var activateLink = function(elem) {
+    $(".active").removeClass("active");
+    elem.addClass("active");
+  };
+
   var tabsWrapper = $(".tabs");
   var tabs = tabsWrapper.children("div");
   var tabLinks = tabsWrapper.find(".tab-link");
@@ -8,18 +14,16 @@ var tabularize = function() {
   if(active) {
     tabs.hide();
     $(active).show();
-    $(".active").removeClass("active");
     $(".tab-link").each(function() {
       if($(this).attr("href") === active) {
-        $(this).parent().addClass("active");
+        activateLink($(this).parent());
       }
     });
   }
   tabLinks.click(function() {
     tabs.hide();
     $($(this).attr("href")).show();
-    $(".active").removeClass("active");
-    $(this).parent().addClass("active");
+    activateLink($(this).parent());
     return false;
   });
 };
